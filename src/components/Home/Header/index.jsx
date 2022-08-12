a5a8eeb31f1a1ecdab6c3f560e41fde2136afb47
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2022-08-13 00:37:47 +0530

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => (
+  <header className="home-header">
+    <h1>
+      <span>“ Sambit's</span> Blog <span>”</span>
+    </h1>
+    <p>KEEP UP TO DATE ON WEB DEV</p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,28 @@
+// TO Add code add <pre></pre> TAg
+export const blogList = [
+  {
+    id: 1,
+    title: "7 CSS tools you should be using ",
+    category: "development",
+    subCategory: ["frontend", "ui/ux", "design"],
+    description: `
+### As a web developer, writing CSS code is always one of the major tasks you would do on a daily basis. But CSS is an awesome stylesheet language that allows us to style web pages and make them interactive on all user devices. Just imagine a web page without CSS code. There is a lot that we can do with this amazing stylesheet language.
+<br/>
+### However, sometimes writing good CSS code from scratch can take a lot of time, especially if you’re working on a large project. But fortunately, there are a lot of CSS tools available on the web for free. These tools can be a lot helpful to save some time and boost our coding productivity as developers.
+<br/>
+
+## 1. Neumorphism
+## 2. CSS Clip-path Maker
+## 3. Animista
+## 4. PurgeCSS
+## 5. Shadow Brumm
+## 6. CSS Gradient
+## 7. CSS Scan
+
+      `,
+    authorName: "SamNanda",
+    authorAvatar: "/assets/images/sambit.jpeg",
+    createdAt: "Aug 07, 2022",
+    cover: "/assets/images/designer-1.jpg",
+  },
+];
